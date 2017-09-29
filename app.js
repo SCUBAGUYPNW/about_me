e3a342f0d9dbdefaa540cfe8370453fc41a36ae1
Mitchel Hall <scubaphoto@gmail.com>
2017-09-28 21:17:57 -0700

    added comments to app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// This will be a five question guessing game.
+// The questions will be a mix of Yes/No answers. User input must accept either y/n or Yes/No responses. toUpperCase() or toLowerCase()
+// will be used to validate user input and accommodate user enter all-caps answers as well.
+// Include usefull and well written console.log() messages
+// correctly display to the brower console for each question of the guessing game.
