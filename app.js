2be6bb4bf09accbda48f78b4b75f0f4294c2df0b
Mitchel Hall <scubaphoto@gmail.com>
2017-09-30 22:41:22 -0700

    Ready to submit lab 03 assignment

--- app.js
+++ app.js
@@ -19,11 +19,10 @@ console.log ('Player entered answer ' + firstQuestion + '.');
 
 // Evaluate player answer.
 
-
 if (firstQuestion === 'No' || firstQuestion === 'N') {
   alert ('Great Guess! ' + playerName + ' Riding the bus saves me from having to find parking in Seattle.');
   playerScore++ ;
-}  else {
+} else {
   alert ('Sorry, wrong answer, ' + playerName + '.' + ' Riding the bus saves me from having to find parking in Seattle.' );
 }
 alert ('Your score is ' + playerScore + ' ' + playerName + '.' );
@@ -32,39 +31,60 @@ alert ('Your score is ' + playerScore + ' ' + playerName + '.' );
 
 var secondQuestion = prompt ('Do I like to play baseball ' + playerName + '?' + ' Please answer either Yes or No').toUpperCase();
 console.log ('Player entered secondQuestion answer as ' + secondQuestion + '.');
-
-  if (secondQuestion === 'No' || secondQuestion === 'N'); {
-    alert ('Great Guess! ' + playerName + 'I haven\'t played baseball since Highschool.');
-    playerScore++ ;
-  } else {
-    alert ('Sorry, wrong answer, ' + playerName + ' I haven\'t played baseball since Highshcool.');
-    }
+if (secondQuestion === 'No' || secondQuestion === 'N') {
+  alert ('Great Guess! ' + playerName + 'I haven\'t played baseball since Highschool.');
+  playerScore++ ;
+} else {
+  alert ('Sorry, wrong answer, ' + playerName + ' I haven\'t played baseball since Highshcool.');
+}
 alert ('Your score is ' + playerScore + ' ' + playerName + '.' );
 
-var thirdQuestion = prompt 'Do I like to play the guitar, ' + playerName + '?' + 'Please answer either Yes or No').toUpperCase();
+// Ask player their third question.
+
+var thirdQuestion = prompt ('Do I like to play the guitar, ' + playerName + '?' + 'Please answer either Yes or No').toUpperCase();
 console.log ('Player entered answer ' + secondQuestion + '.');
-
-if (thirdQuestion = 'No' || thirdQuestion 'N') {
-    alert ('Great Guess! ' + playerName + 'I unfortunately do not \(currently anyway\) know how to play any musical instrament.');
-    playerScore++
-    } else {
-    alert ('Sorry, wrong answer, ' + playerName + ' I unfortunately do not \(currently anyway\) know how to play any musical instrament.');
-    }
-
-  var fourthQuestion = prompt ('Do I like to SCUBA dive, ' + playerName + '?' + 'Please answer either Yes or No').toUpperCase();
-        console.log ('Player entered answer ' + fourthQuestion + '.');
-          if (fourthQuestion = 'Yes' || 'Y') {
-            alert ('Great Guess! ' + playerName + 'I haven\'t been diving in a long time but hope to do so again soon.');
-      playerScore++
-            } else {
-            alert ('Sorry, wrong answer, ' + playerName + ' I actually love to go SCUBA diving, it has taken me all over the world.')
-            }
-
+if (thirdQuestion === 'No' || thirdQuestion === 'N' ) {
+  alert ('That is correct ' + playerName + 'I unfortunately do not /(as of yet/) play any musical instrament.')
+} else {
+  alert ('Actually ' + playerName + ' I never learned to play any musical instrament.');
+var fourthQuestion = prompt ('Do I like to SCUBA dive ' + playerName + '?');
+if (fourthQuestion === 'Yes' || fourthQuestion === 'Y') {
+  alert ('You know me well ' + playerName + ' I used to SCUBA dive regularly, I miss it very much and cannot wait to get into the water again');
+} else {
+  alert ('Sorry ' + playerName + ' that is the wrong answer. I actually love SCUBA diving.');
+}
 var fifthQuestion = prompt ('Do I like to read mystery novels ' + playerName + '?' + 'Please answer either Yes or No').toUpperCase();
-    console.log ('Player entered answer ' +fifthQuestion + '.');
-      if (fithQuestion = 'No' || 'N') {
-          alert ('Great Guess! ' + playerName + ' I have read a few mystery novels, but I would much rather read Sci-fi.' );
-      playerScore++
-          } else {
-          alert ('Sorry, wrong answer, ' + playerName + ' I have read a few mystery novels, but I would much rather read Sci-fi.')
-                }
+console.log ('Player entered answer ' + fifthQuestion + '.');
+if (fithQuestion === 'No' || fithQuestion === 'N') {
+  alert ('Great Guess! ' + playerName + ' I have read a few mystery novels, but I would much rather read Sci-fi.' );
+  playerScore++;
+} else {
+  alert ('Sorry, wrong answer, ' + playerName + ' I have read a few mystery novels, but I would much rather read Sci-fi.')
+}
+
+// Question # 6
+
+// var userGuess = prompt ('Hello, can you guess my number? Please enter a number between 1 and 200.');
+// var userNumberConverted = parseInt(userGuess);
+// var guessCount = 0;
+// var highValue = 200;
+// var lowValue = 1;
+//
+// while (userNumberConverted != 42) {
+//   userNumberConverted = parseInt(userGuess);
+//   console.log ('Player entered ' + userGuess + '.' );
+//   guessCount++;
+//   if (userNumberConverted < 42){
+//     lowValue = userNumberConverted;
+//     alert ('You\'re guess was incorrect. Please guess a higher number');
+//     userGuess = prompt ('Please guess again, enter a number between ' + userNumberConverted + ' and ' + highValue + '.' );
+//   } else if (userNumberConverted > 42){
+//     highValue = userNumberConverted;
+//     alert ('You\'re guess was incorrect. Please guess a lower number');
+//     userGuess = prompt ('Please guess again, enter a number between ' + lowValue + ' and ' + userNumberConverted + '.' );
+//   }
+// if (guessCount === 0 && userNumberConverted === 42) {
+//   alert ('congratulations, you guess correct on the first try!');
+// } else {
+//   alert ('congratulations, you guessed the correct number in ' + guessCount + '.');
+// }
